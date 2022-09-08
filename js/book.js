e50cba91a994b8354d0fc5234812233787992fa7
Lester Lee <Lester.Lee@fullstackacademy.com>
2022-09-08 16:59:39 -0400

    write unit 4 solution

--- js/book.js
+++ js/book.js
@@ -1,10 +1,13 @@
-const Book = (author, language, subject, title) => {
+function Book(author, language, subject, title) {
   this.author = author;
   this.language = language;
   this.subject = subject;
   this.title = title;
 
+  /* CHANGE RENDER! THIS IS A TEMPLATE */
   this.render = () => {
-    
+    const li = document.createElement('li');
+    li.textContent = this.title;
+    return li; 
   }
 };
--- js/bookshelf.js
+++ js/bookshelf.js
@@ -1,9 +1,27 @@
-const Bookshelf = (books = []) => {
+function Bookshelf(books = []) {
   this.books = books;
 
-  this.addBook = (book) => this.books.push(book);
+  this.addBook = function (book) {
+    this.books.push(book);
+  };
 
-  this.removeBook = (book) => {
-    const idx = this.books.map(b => b.title[0]).indexOf(book.title[0]);
-  }
+  this.removeBook = function (book) {
+    // Find a book with the same title
+    const idx = this.books.map((b) => b.title).indexOf(book.title);
+    if (idx !== -1) {
+      this.books.splice(idx, 1);
+      return book;
+    } else {
+      return null;
+    }
+  };
+
+  /* CHANGE RENDER! THIS IS A TEMPLATE */
+  this.render = function () {
+    const ul = document.createElement("ul");
+    for (const book of this.books) {
+      ul.append(book.render());
+    }
+    return ul;
+  };
 }
